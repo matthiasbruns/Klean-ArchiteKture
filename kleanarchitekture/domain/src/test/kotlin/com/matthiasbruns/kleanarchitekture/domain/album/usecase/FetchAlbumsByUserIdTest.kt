682629c04b960b644f603830707d8b2fa7570c89
Matthias Bruns <matthias.bruns@rewe-digital.com>
2018-02-21 09:21:44 +0100

    Add a domain test

--- kleanarchitekture/domain/src/test/kotlin/com/matthiasbruns/kleanarchitekture/domain/album/usecase/FetchAlbumsByUserIdTest.kt
+++ kleanarchitekture/domain/src/test/kotlin/com/matthiasbruns/kleanarchitekture/domain/album/usecase/FetchAlbumsByUserIdTest.kt
@@ -2,7 +2,6 @@ package com.matthiasbruns.kleanarchitekture.domain.album.usecase
 
 import com.matthiasbruns.kleanarchitekture.domain.album.AlbumRepository
 import com.matthiasbruns.kleanarchitekture.domain.album.model.Album
-import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import io.kotlintest.specs.WordSpec
@@ -17,7 +16,6 @@ class FetchAlbumsByUserIdTest : WordSpec() {
         get() = mock()
 
     init {
-
         "FetchAlbumsByUserId.execute" should {
 
             "return the same object as the one returned from the AlbumRepository "{
@@ -25,14 +23,20 @@ class FetchAlbumsByUserIdTest : WordSpec() {
                 val useCase = FetchAlbumsByUserId(repository)
                 val albumList = listOf<Album>()
 
-                whenever(repository.fetchByUser(any())).thenReturn(Single.just(albumList))
+                whenever(repository.fetchByUser(1)).thenReturn(Single.just(albumList))
+                val element = Album(id = 1, userId = 1, photos = emptyList(), title = "GOOD ALBUM")
+                whenever(repository.fetchByUser(2)).thenReturn(Single.just(listOf(element)))
 
                 useCase.execute(1).test()
-                        .assertValue { albumList === albumList }
+                        .assertValue { it === albumList }
                         .assertComplete()
+
+                useCase.execute(2).test()
+                        .assertValue { it.size == 1 }
+                        .assertValue { it.first() === element }
+                        .assertComplete()
+
             }
-
         }
-
     }
 }
